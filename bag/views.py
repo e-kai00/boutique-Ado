331ec053760a8576e49cc097cd5d0bbd01bc638b
Katerina <yelkaterina@gmail.com>
2023-07-04 20:26:55 +0200

    Product size logic

--- bag/views.py
+++ bag/views.py
@@ -12,12 +12,30 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if size:
+        # if item already in bag
+        if item_id in list(bag.keys()):
+            # if item of the same id and size already exist
+            if size in bag[item_id]['items_by_size'].keys():
+                # increment quantity
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        # if item not in bag
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     
