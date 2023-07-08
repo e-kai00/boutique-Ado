52476fda3486f03109a234d50fea569154b1ea51
Katerina <yelkaterina@gmail.com>
2023-07-08 15:41:28 +0200

    Add user history

--- checkout/views.py
+++ checkout/views.py
@@ -5,7 +5,10 @@ from django.conf import settings
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+
 from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from bag.contexts import bag_contents
 
 import stripe
@@ -28,6 +31,7 @@ def cache_checkout_data(request):
         messages.error(request, 'Sorry, your payment cannot be \
             processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -89,26 +93,46 @@ def checkout(request):
             messages.error(request, 'There was an error with your form. \
                 Please double check your information.')
 
-    # get the bag from session and if empty, redirect user to products page
-    bag = request.session.get('bag', {})
-    if not bag: 
-        messages.error(request, 'There is nothing in your bag at the moment')
-        return redirect(reverse('products'))
-    
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
-    # stripe will require the amount to charge as an integer
-    stripe_total = round(total * 100)
-    # set key on stripe
-    stripe.api_key = stripe_secret_key
-    # create Payment intent
-    intent = stripe.PaymentIntent.create(
-        amount = stripe_total,
-        currency = settings.STRIPE_CURRENCY
-    )
+    else:
+        # get the bag from session and if empty, redirect user to products page
+        bag = request.session.get('bag', {})
+        if not bag: 
+            messages.error(request, 'There is nothing in your bag at the moment')
+            return redirect(reverse('products'))
         
-    order_form = OrderForm()
+        current_bag = bag_contents(request)
+        total = current_bag['grand_total']
+        # stripe will require the amount to charge as an integer
+        stripe_total = round(total * 100)
+        # set key on stripe
+        stripe.api_key = stripe_secret_key
+        # create Payment intent
+        intent = stripe.PaymentIntent.create(
+            amount = stripe_total,
+            currency = settings.STRIPE_CURRENCY
+        )
 
+        # prefill form for authenticated users
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()        
+        
+       
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing')
 
@@ -129,6 +153,28 @@ def checkout_success(request, order_number):
     # check whether user wanted to save their information
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+   
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
